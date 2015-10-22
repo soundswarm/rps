c4105d3ce7ef8e4aa94db8b1e33bae2b81521415
Sean Connor <seantconnor@gmail.com>
2015-10-21 20:09:30 -0700

    added new game and reset score buttons.  cleaned up code

--- rps.js
+++ rps.js
@@ -1,33 +1,30 @@
 Game = new Mongo.Collection("game");
 
 if (Meteor.isClient) {
+  Template.body.events({
+    'click .new-game': function() {
+      event.preventDefault();
+      Meteor.call('newGame');
+    },
+    'click .reset-scores': function() {
+      event.preventDefault();
+      Meteor.call('resetScores');
+    }
+  });
 
   Template.player1.events({
     'click .rps': function (event) {
-      console.log();
-      var choice = $(event.target).data().rps;;
-      var id = Game.find({name: 'player1'}).fetch()[0]._id;
-      // Game.update(id, {$inc: {score: 1}});
-      Game.update(id, {$set: {choice: choice}});
-      Game.update(id, {$set: {status: 'thrown'}});
-      console.log(id);
-
-      // check winner   
-      var winner = pickWinner();
-      return winner
+      var choice = $(event.target).data().rps;
+      var id = Game.findOne({name: 'player1'})._id;
+      setThrow(id, choice);
     }
   });
 
   Template.player2.events({
     'click .rps': function (event) {
-      console.log();
-      var choice = $(event.target).data().rps;;
-      var id = Game.find({name: 'player2'}).fetch()[0]._id;
-      // Game.update(id, {$inc: {score: 1}});
-      Game.update(id, {$set: {choice: choice}});
-      Game.update(id, {$set: {status: 'thrown'}});
-      console.log(id);
-      var winner = pickWinner();
+      var choice = $(event.target).data().rps;
+      var id = Game.findOne({name: 'player2'})._id;
+      setThrow(id, choice);
     }
   });
 
@@ -37,31 +34,39 @@ if (Meteor.isClient) {
   Template.body.helpers({
     players: function() {
       return Game.find({});
-    },
-    winner: function() {
-      return Session.get('winner');
     }
   });
 
+  var setThrow = function(id, choice) {
+    Game.update(id, {$set: {choice: choice}});
+    Game.update(id, {$set: {status: 'thrown'}});
+    pickWinner();
+  };
+
   var pickWinner = function() {
-    var player1Id = Game.find({name: 'player1'}).fetch()[0]._id;
-    var player2Id = Game.find({name: 'player2'}).fetch()[0]._id;
-    var player1Choice = Game.find({name: 'player1'}).fetch()[0].choice;
-    var player2Choice = Game.find({name: 'player2'}).fetch()[0].choice;
+    var player1 = Game.findOne({name: 'player1'});
+    var player2 = Game.findOne({name: 'player2'});
+
     var winMap = {
       rock: 'paper',
       paper: 'scissors',
       scissors: 'rock'
     };
+    if(player1.status === 'thrown' && player2.status === 'thrown') {
+      if(winMap[player1.choice] === player2.choice) {
+        Game.update(player2._id, {$inc: {score: 1}});
+        
+        Game.update(player1._id, {$set: {message: 'Player 2 Wins!'}});
+        Game.update(player2._id, {$set: {message: 'Player 2 Wins!'}});
+      } else if (player1.choice === player2.choice) {
+        Game.update(player1._id, {$set: {message: 'Tie'}});
+        Game.update(player2._id, {$set: {message: 'Tie'}});
+      } else {
+        Game.update(player1._id, {$inc: {score: 1}});
 
-    if(winMap[player1Choice] === player2Choice) {
-      Game.update(player2Id, {$inc: {score: 1}});
-      Session.set('winner', 'Player 2 Wins!');
-    } else if (player1Choice === player2Choice) {
-       Session.set('winner', 'tied');
-    } else {
-      Game.update(player1Id, {$inc: {score: 1}});
-       Session.set('winner', 'Player 1 Wins!');
+        Game.update(player1._id, {$set: {message: 'Player 1 Wins!'}});
+        Game.update(player2._id, {$set: {message: 'Player 1 Wins!'}});
+      }
     }
   };
 }
@@ -76,25 +81,32 @@ if (Meteor.isServer) {
         name: 'player1',
         score: 0,
         choice: null,
-        status: 'not thrown'
+        status: 'not thrown',
+        message: null
       });
 
       Game.insert({
         name: 'player2',
         score: 0,
         choice: null,
-        status: 'not thrown'
+        status: 'not thrown',
+        message: null
       });
     }
   });
 }
 
 Meteor.methods({
-  setThrow: function(choice, player) {
-    return Game.insert({
-        name: player,
-        choice: choice
-    });
+  'resetScores': function() {
+    Game.update({}, {$set: {score: 0}}, {multi: true});
+  },
+  'newGame': function() {
+    var obj = {
+      choice: null,
+      status: 'not thrown',
+      message: null
+    };
+    Game.update({}, {$set: obj}, {multi: true});
   }
 });
 
